845adb6d56fb3b35c3c5ee539852c0dd59bdb185
Yulai202020 <ngmyulai@gmail.com>
2025-02-02 15:33:31 +0400

    first commit

--- quiz/src/index.js
+++ quiz/src/index.js
@@ -23,16 +23,40 @@ async function printFiglet(string) {
     });
 }
 
-async function load(text, seconds = 2000) {
+async function checkAnswers(
+    isCorrect,
+    text = 'Checking answer',
+    seconds = 2000
+) {
     const spiner = createSpinner(text).start();
     await sleep(seconds);
-    spiner.stop();
+
+    if (isCorrect) {
+        spiner.success({ text: 'Nice work!' });
+        return true;
+    } else {
+        spiner.error({ text: 'Game over...' });
+        process.exit(1);
+    }
+}
+
+async function ask_list(question, choices, true_ans) {
+    const anwers = await inquirer.prompt({
+        name: 'output',
+        type: 'list',
+        message: question,
+        choices: choices,
+    });
+
+    await checkAnswers(anwers.output === true_ans);
 }
 
 await printAnimated('Math Quiz!!');
 
-// ask questions
-
-await load('Checking answers');
+await ask_list('1+1=?', ['idk', '2', '3'], '2');
+await ask_list('2+10=?', ['idk', '8', '12'], '12');
+await ask_list('3*2=?', ['idk', '6', '9'], '6');
+await ask_list('6*6=?', ['idk', '12', '36'], '36');
+await ask_list('sqrt(25)=?', ['idk', '1', '-1'], '5');
 
 await printFiglet('You won $1000!!!');
