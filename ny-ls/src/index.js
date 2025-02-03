08a861892c7667679bcde8a4ef41bef633e562fd
Yulai202020 <ngmyulai@gmail.com>
2025-02-03 11:53:06 +0400

    first commit

--- ny-ls/src/index.js
+++ ny-ls/src/index.js
@@ -1,44 +1,84 @@
 #!/usr/bin/env node
 
-import fs from 'node:fs';
+import os from 'node:os';
 import path from 'node:path';
+import fs from 'node:fs/promises';
+
 import chalk from 'chalk';
 import { program } from 'commander';
 
-program.argument('<dirs...>');
+async function listDir(dir) {
+    try {
+        return await fs.readdir(dir);
+    } catch (err) {
+        console.error('Error occurred while reading directory:', err);
+    }
+}
+
+function modeToString(mode) {
+    const fileType = (mode & 0o170000) === 0o040000 ? 'd' : '-';
+    const ownerPermissions = [
+        mode & 0o400 ? 'r' : '-',
+        mode & 0o200 ? 'w' : '-',
+        mode & 0o100 ? 'x' : '-',
+    ].join('');
+    const groupPermissions = [
+        mode & 0o040 ? 'r' : '-',
+        mode & 0o020 ? 'w' : '-',
+        mode & 0o010 ? 'x' : '-',
+    ].join('');
+    const othersPermissions = [
+        mode & 0o004 ? 'r' : '-',
+        mode & 0o002 ? 'w' : '-',
+        mode & 0o001 ? 'x' : '-',
+    ].join('');
+
+    return `${fileType}${ownerPermissions}${groupPermissions}${othersPermissions}`;
+}
+
+function sizeToString(size) {
+    let postfixs = ['B', 'Kb', 'Mb', 'Gb', 'Pb'];
+    let now_postfix = 'B';
+
+    for (let id = 0; id < postfixs.length; id++) {
+        if (size < 1024 || id === postfixs.length - 1) {
+            return `${size.toFixed(1)}${now_postfix}`;
+        }
+
+        size /= 1024;
+        now_postfix = postfixs[id + 1];
+    }
+}
+
+program.argument('<dirs...>').version('1.0.0');
 
 program.parse(process.argv);
 
 const argument = program.args;
 
-for (let dirPath of argument) {
-    fs.readdir(dirPath, (err, files) => {
-        if (err) {
-            console.error(
-                chalk.red(`Error reading directory: ${dirPath}`),
-                err.message
-            );
-            return;
+for (let [id, dirPath] of argument.entries()) {
+    console.log(dirPath, ':');
+
+    const files = await listDir(dirPath);
+
+    for (let file of files) {
+        const filename = path.join(dirPath, file);
+
+        const stat = await fs.stat(filename);
+
+        const owner = os.userInfo(stat.uid).username;
+        const group = os.userInfo(stat.gid).username;
+        const mode = modeToString(stat.mode);
+        const size = sizeToString(stat.size);
+
+        if (stat.isDirectory()) {
+            console.log(mode, owner, group, size, chalk.green(file));
+        } else {
+            console.log(mode, owner, group, size, file);
         }
+    }
 
-        for (let file of files) {
-            const filename = path.join(dirPath, file);
-
-            fs.stat(filename, (err, stat) => {
-                if (err) {
-                    console.error(
-                        chalk.red(`Error reading file: ${filePath}`),
-                        err.message
-                    );
-                    return;
-                }
-
-                if (stat.isDirectory()) {
-                    console.log(chalk.green(file));
-                } else {
-                    console.log(file);
-                }
-            });
-        }
-    });
+    if (id !== argument.length - 1) {
+        console.log();
+    }
 }
