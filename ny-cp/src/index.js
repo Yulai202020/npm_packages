930157a944f75e8a7ea39019d71e0e26b363141a
Yulai202020 <ngmyulai@gmail.com>
2025-02-04 16:05:32 +0400

    first commit

--- ny-cp/src/index.js
+++ ny-cp/src/index.js
@@ -6,6 +6,14 @@ import fs from 'node:fs/promises';
 
 import { program } from 'commander';
 
+async function cpFile(from, to) {
+    try {
+        return await fs.copyFile(from, to);
+    } catch (err) {
+        console.error('Error occurred while reading directory:', err);
+    }
+}
+
 program.argument('<from>').argument('<to>').version('1.0.0');
 
 program.parse(process.argv);
@@ -15,4 +23,12 @@ const argument = program.args;
 const from = argument[0];
 const to = argument[1];
 
-const error = await fs.copyFile(from, to);
+if (fs.existsSync(from)) {
+    if (fs.existsSync(to)) {
+        throw new Error('You cant copy file to existing file.');
+    }
+
+    await cpFile(from, to);
+} else {
+    throw new Error('Cannot copy file from doesnt exist file.');
+}
