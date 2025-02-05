4c5f77c16b2572dc4efbb01eb6578ffd297a2e9c
Yulai202020 <ngmyulai@gmail.com>
2025-02-05 11:24:39 +0400

    done program

--- ny-cp/src/index.js
+++ ny-cp/src/index.js
@@ -1,34 +1,44 @@
 #!/usr/bin/env node
 
-import os from 'node:os';
-import path from 'node:path';
 import fs from 'node:fs/promises';
 
 import { program } from 'commander';
 
-async function cpFile(from, to) {
+async function isExists(filepath) {
     try {
-        return await fs.copyFile(from, to);
-    } catch (err) {
-        console.error('Error occurred while reading directory:', err);
+        const stats = await fs.stat(filepath);
+        return true;
+    } catch (error) {
+        return false;
     }
 }
 
-program.argument('<from>').argument('<to>').version('1.0.0');
+program
+    .argument('<from>')
+    .argument('<to>')
+    .option('-r, --recursive', '')
+    .version('1.0.0');
 
 program.parse(process.argv);
 
 const argument = program.args;
+const options = program.opts();
 
 const from = argument[0];
 const to = argument[1];
 
-if (fs.existsSync(from)) {
-    if (fs.existsSync(to)) {
-        throw new Error('You cant copy file to existing file.');
-    }
+const isRecursive = options.recursive ? true : false;
 
-    await cpFile(from, to);
-} else {
+if (!(await isExists(from))) {
     throw new Error('Cannot copy file from doesnt exist file.');
+} else if (await isExists(to)) {
+    throw new Error('You cant copy file to existing file.');
 }
+
+const stats = await fs.stat(from);
+
+if (stats.isDirectory() && !isRecursive) {
+    throw new Error(`cp: -r not specified; omitting directory '${from}'`);
+}
+
+await fs.cp(from, to, { recursive: isRecursive });
